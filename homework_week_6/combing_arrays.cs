65b354b9c4802bccfac6df841584443eae7a2594
BenKevin harris <70765072+ben-har@users.noreply.github.com>
2025-03-30 11:48:06 -0500

    Add files via upload

---
+++ homework_week_6/combing_arrays.cs
@@ -0,0 +1,47 @@
+﻿namespace CombingArrays
+{
+    public class combineClass
+    {
+        public List<int> Combine(List<int> listOne, List<int> listTwo)
+        {
+            List<int> result = new List<int> { };
+
+            listOne.Sort();
+            listTwo.Sort();
+
+            for(int i = 0;  i < listOne.Count; i++)
+            {
+                result.Add(listOne[i]);
+            }
+            for(int b = 0; b < listTwo.Count; b++)
+            {
+                result.Add(listTwo[b]);
+
+            }
+           
+            for(int c = 0; c < result.Count; c++)
+            {
+                Console.WriteLine(result[c]);
+            }
+           
+            return result;
+        }
+
+
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            combineClass Start = new combineClass();
+
+            List <int> lOne = new List<int> { 3,1,2,4};
+
+            List <int> lTwo = new List<int> { 5, 6, 7, 10 };
+
+           Start.Combine(lOne, lTwo);
+
+        }
+    }
+}
