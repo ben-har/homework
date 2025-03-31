1ff04fb0a48621247866e9fd5665153f247f5ad4
BenKevin harris <70765072+ben-har@users.noreply.github.com>
2025-03-31 12:14:28 -0500

    Add files via upload

---
+++ homework_week_6/RemoveVar.cs
@@ -0,0 +1,50 @@
+﻿namespace RemoveVar
+{
+    public class ClassHolder
+    {
+        public int elementRemover(string[] Holder, int val)
+        {
+            int Remaining = Holder.Length;
+
+            for (int i = 0; i < Holder.Length; i++)
+            {
+                if (Holder[i] == val.ToString())
+                {
+                    Holder[i] = "_";
+                    Remaining--;
+
+                }
+
+
+
+
+            }
+            return Remaining;
+
+        }
+        
+
+
+
+
+
+
+    }
+    
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            ClassHolder myClassholder = new ClassHolder();
+
+            Console.WriteLine(myClassholder.elementRemover(["1","3","6","7","5","6"], 6));
+        }
+    }
+}
+
+
+
+
+   
