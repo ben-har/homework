878b2fa4fcab06cd10dc504de4001fa58d1c292c
BenKevin harris <70765072+ben-har@users.noreply.github.com>
2025-03-31 11:58:16 -0500

    Add files via upload

---
+++ homework_week_6/Haystack.cs
@@ -0,0 +1,40 @@
+﻿namespace NeedleInHayStack
+{
+    public class ClassHolder
+    {
+        public int HayStackFinder(string Haystack, string Needle)
+        {
+            for(int i = 0; i < Haystack.Length -1; i++)
+            {
+                if (Haystack[i] == Needle[0])
+                    return i;
+
+
+
+
+            }
+            return -1;
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassHolder myClass = new ClassHolder();
+
+            Console.WriteLine(myClass.HayStackFinder("Ben","e"));
+        }
+    }
+}
