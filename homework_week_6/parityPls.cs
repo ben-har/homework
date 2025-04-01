541fd146c0a4d600437d2e7425fb0af5adeaba8f
BenKevin harris <70765072+ben-har@users.noreply.github.com>
2025-04-01 13:11:00 -0500

    Add files via upload

---
+++ homework_week_6/parityPls.cs
@@ -0,0 +1,65 @@
+﻿namespace Parity
+{
+    internal class Program
+    {
+        public class ClassHolder
+        {
+            public int[] SortArray(int[] array)
+            {
+                int Temp;
+                
+                //this needs to be a nested for loop 
+                for(int i = 0; i < array.Length; i++)
+                { 
+                   for(int j = 0; j < array.Length -1; j++)
+                    {
+                    // have this move over the Character if it is not even
+                        int Even = array[j] % 2;
+                        int EvenNext = array[j + 1] % 2; 
+                        if (Even == 1 && EvenNext == 0)
+                        {
+                             //4 5
+                             // temp = 4
+                            Temp = array[j];
+
+                           
+                            array[j] = array[j + 1];
+                            array[j + 1] = Temp;
+
+                            
+                           
+                            
+
+                        }
+
+
+                   }
+                }
+                return array;
+
+
+
+
+            }
+
+
+
+
+
+
+        }
+        static void Main(string[] args)
+        {
+            ClassHolder myclass = new ClassHolder();
+
+
+            int[] numbers = { 1, 2, 3 , 4, 5 ,6 ,7  };
+
+            int[] SortArray = myclass.SortArray(numbers);
+
+            Console.WriteLine(string.Join(", ", numbers));
+
+
+        }
+    }
+}
